dab7a67f38314b08777c29ccdbdc3794df6d74fb
Rebecca Crecelius <info@rebeccacrecelius.com>
2024-03-31 14:35:04 +0000

    Fix media queries add JavaScript file Fix CTA media queries for viewability on smaller screensizes Add h2 tag to features Hid detailed information for features so it would appear on click Add JavaScript file for interactivity on features section

---
+++ scripts/twemary.js
@@ -0,0 +1,13 @@
+let hideOne = document.getElementById("textOne");
+let hideTwo = document.getElementById("textTwo");
+let hideThree = document.getElementById("textThree");
+let hideFour = document.getElementById("textFour");
+
+hideOne.hidden = true;
+hideTwo.hidden = true;
+hideThree.hidden = true;
+hideFour.hidden = true;
+
+hideOne.onclick = function() {
+	hideOne.hidden = false;
+}

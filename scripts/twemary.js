4b114dc45dc5813b940467c8f1d352a448271035
Rebecca Crecelius <info@rebeccacrecelius.com>
2024-04-02 16:00:46 +0000

    Update features to attempt hiddeen and show text

--- scripts/twemary.js
+++ scripts/twemary.js
@@ -1,14 +1,7 @@
-
-let hideOne = document.getElementById("textOne");
-let hideTwo = document.getElementById("textTwo");
-let hideThree = document.getElementById("textThree");
-let hideFour = document.getElementById("textFour");
-
-hideOne.hidden = true;
-hideTwo.hidden = true;
-hideThree.hidden = true;
-hideFour.hidden = true;
-
-hideOne.onmousehover = function() {
-	hideOne.hidden = false;
-}
+document.getElementById("features").addEventListener(
+	"click",
+	() => {
+	document.getElementById("description").hidden = true;
+	},
+false,
+);

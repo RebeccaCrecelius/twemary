8cb907264a9c95c004e1354042061a4701ae1063
Rebecca Crecelius <info@rebeccacrecelius.com>
2024-04-03 16:03:41 +0000

    Update html and css with semantic tags Update JavaScript with additional attempts

--- scripts/twemary.js
+++ scripts/twemary.js
@@ -1,7 +1,29 @@
-document.getElementById("features").addEventListener(
-	"click",
-	() => {
-	document.getElementById("description").hidden = true;
-	},
-false,
-);
+/*When someone hovers over the picture, the description text appears. 
+When they move away from the picture, the description text disapperas.*/
+
+const picture = ['image-one', 'image-two', 'image-three', 'image-four'];
+
+const info = [];
+
+picture.forEach(function(image-container){
+	info.push(document.getElementById(description));
+})
+
+const infoShow = function(event) {
+	event.target.style.display="block";
+}
+
+const infoHide = function(event) {
+	event.target.style.display='';
+}
+
+let eventAssignemnt = fuction(tell) {
+	tell.onmouseover = function() {
+	infoShow(event);
+}
+	tell.onmouseout =  function() {
+	infoHide(event);
+}
+};
+	
+

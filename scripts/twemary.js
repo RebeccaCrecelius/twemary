799ba160f17434264e109114d3ba06b88c2cc5b6
Rebecca Crecelius <info@rebeccacrecelius.com>
2024-05-20 14:28:05 +0000

    Fix screen resizing issues Update HTML with hamburger code Add code for hamburger in JavaScript file Include code for navigation in contact.html

--- scripts/twemary.js
+++ scripts/twemary.js
@@ -37,3 +37,16 @@ $(document).ready(function () {
         $(this).toggleClass('clicked');
     });
 });
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".main-nav");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
